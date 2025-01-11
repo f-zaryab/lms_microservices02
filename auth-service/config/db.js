4f462d4ab6d33a0ea0dda7380fed7462706b85a7
Zaryab Farooqui <71942618+f-zaryab@users.noreply.github.com>
2025-01-11 22:48:58 +0000

    aws debug002

--- auth-service/config/db.js
+++ auth-service/config/db.js
@@ -1,15 +1,12 @@
 const AWS = require('aws-sdk');
 const DOC = require("dynamodb-doc");
-require('dotenv').config();
 
 // Configure AWS region and credentials
 AWS.config.update({
-    region: "us-east-1",
-    accessKeyId: "ASIAZLECMYTHWAARJX2I",
-    secretAccessKey: "duXBWwe6vx/chP0CIY+rK5E+q0r4StQO5S/Y8VW0"
+    region: "us-east-1"
 });
 
-const dynamoDB = new AWS.DynamoDB({ region: 'us-east-1' });
+const dynamoDB = new AWS.DynamoDB();
 // const docClient = new AWS.DynamoDB.DocumentClient();
 const docClient = new DOC.DynamoDB();
 
